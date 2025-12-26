a6e27b5bba5e95591e11e92ffefe21e839590235
Eric Afes <eriicafes@gmail.com>
2025-12-26 17:45:24 +0100

    Fix changeset publish command

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 NEW_VERSION=$(grep -m 1 '"version"' package.json | awk -F '"' '{print $4}')
-echo "New version for 'tmpl' is v$NEW_VERSION"
+echo "New version for 'wfs' is v$NEW_VERSION"
 git tag v$NEW_VERSION -m "v$NEW_VERSION"
